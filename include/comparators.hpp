54370e34b01931822544105ea3965d1568884406
Vitalii Orazov <viordash@yandex.ru>
2025-08-03 13:31:16 +0000

    book: implementation of book  comparators

--- include/comparators.hpp
+++ include/comparators.hpp
@@ -4,6 +4,16 @@
 
 namespace bookdb::comp {
 
-struct LessByAuthor {};
+struct LessByAuthor {
+    bool operator()(const Book &lhs, const Book &rhs) const { return lhs.author < rhs.author; }
+};
+
+struct LessByRating {
+    bool operator()(const Book &lhs, const Book &rhs) const { return lhs.rating < rhs.rating; }
+};
+
+struct LessByPopularity {
+    bool operator()(const Book &lhs, const Book &rhs) const { return lhs.read_count < rhs.read_count; }
+};
 
 }  // namespace bookdb::comp
