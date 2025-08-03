8bc0b311ad73a10950aaa95ba92c2c97b0dc4f94
Vitalii Orazov <viordash@yandex.ru>
2025-08-03 20:47:27 +0000

    filters: implement RatingAbove && GenreIs && all_of && any_of

--- include/filters.hpp
+++ include/filters.hpp
@@ -10,6 +10,24 @@ inline auto YearBetween(int min, int max) {
     return [min, max](const Book &book) { return book.year >= min && book.year <= max; };
 }
 
+inline auto RatingAbove(double min_rating) {
+    return [min_rating](const Book &book) { return book.rating >= min_rating; };
+}
+
+inline auto GenreIs(Genre genre) {
+    return [genre](const Book &book) { return book.genre == genre; };
+}
+
+template <BookPredicate... Preds>
+auto all_of(Preds... preds) {
+    return [=](const Book &book) { return (preds(book) && ...); };
+}
+
+template <BookPredicate... Preds>
+auto any_of(Preds... preds) {
+    return [=](const Book &book) { return (preds(book) || ...); };
+}
+
 template <BookIterator I, BookSentinel<I> S, BookPredicate P>
 auto filterBooks(I first, S last, P pred) {
     std::vector<std::reference_wrapper<const Book>> result;
